afd4cc73ea60bdd94a1505e38482a1b8b1cc8acc
Roberto Antelo <roberto_antelo@hotmail.com>
2016-01-05 11:35:31 -0600

    Add Sessions Limitable with DeviseSecurityExtension

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,5 +2,14 @@ class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable
+         :recoverable, :rememberable, :trackable, :validatable,
+         :lockable, :timeoutable, :session_limitable
+
+  # Custom fields valitations
+  validates :name,      presence: true
+  validates :last_name, presence: true
+  validates :phone,     presence: true
+  validates :area,      presence: true
+  validates :puesto,    presence: true
+
 end
