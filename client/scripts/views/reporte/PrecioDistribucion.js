8a5b6be75f821bb9bb79a48448b4b0924ddfdbf6
Lucía Monterrubio <lmonterrubio@intelimetrica.com>
2016-01-12 09:17:04 -0600

    Correct position of text on precio distribucion

--- client/scripts/views/reporte/PrecioDistribucion.js
+++ client/scripts/views/reporte/PrecioDistribucion.js
@@ -10,16 +10,17 @@ class PrecioDistribucion extends React.Component {
         <div style={{
           marginTop: '20px',
           display: 'flex',
-          justifyContent: 'space-around',
-          alignItems: 'center'
+          justifyContent: 'space-between',
+          alignItems: 'center',
+          margin: '10px 35px 0px 35px'
         }}>
           <div style={{textAlign: 'center'}}>
             <p className={'secondary-price'}>$850,000</p>
-            <p className={'subtitle'}>Precio máximo</p>
+            <p className={'subtitle'}>Precio mínimo</p>
           </div>
           <div style={{textAlign: 'center'}}>
             <p className={'secondary-price'}>$2,350,000</p>
-            <p className={'subtitle'}>Precio minimo</p>
+            <p className={'subtitle'}>Precio máximo </p>
           </div>
         </div>
       </div>
