6607c106609a3f55b87675e469e2913239daec0d
Omar Eduardo <torres.omar.eduardo@gmail.com>
2016-01-14 11:28:57 -0600

    Include custom pick bootstrap sass

--- webpack.config.js
+++ webpack.config.js
@@ -60,7 +60,11 @@ module.exports = {
         test: /\.(png|jpg)$/,
         node_modules: /node_modules/,
         loader: 'url-loader?limit=1000'
-      }
+      },
+      {test: /\.woff(2)?(\?v=\d+\.\d+\.\d+)?$/, loader: "url?limit=10000&mimetype=application/font-woff" },
+      {test: /\.ttf(\?v=\d+\.\d+\.\d+)?$/, loader: "url?limit=10000&mimetype=application/octet-stream" },
+      {test: /\.eot(\?v=\d+\.\d+\.\d+)?$/, loader: "file" },
+      {test: /\.svg(\?v=\d+\.\d+\.\d+)?$/, loader: "url?limit=10000&mimetype=image/svg+xml" }
     ]
   },
   stats: {
