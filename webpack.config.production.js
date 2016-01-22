0f7305fec91dc284e1387a91e0ca80456e181703
Omar Eduardo <torres.omar.eduardo@gmail.com>
2016-01-22 16:36:02 -0600

    Include production asset configuration.

---
+++ webpack.config.production.js
@@ -0,0 +1,74 @@
+var path = require('path');
+var webpack = require('webpack');
+
+var node_modules = path.resolve(__dirname, 'node_modules');
+var client = path.resolve(__dirname, 'client');
+var bootstrap = path.resolve(__dirname, 'node_modules/bootstrap/dist/css');
+
+var modulePaths = [node_modules, client, bootstrap];
+
+// Add gzip
+var extractTextPlugin = require('extract-text-webpack-plugin');
+
+module.exports = {
+  entry: {
+    main: path.resolve(__dirname, './client/scripts/main.js'),
+    vendor: ['jquery', 'bootstrap']
+  },
+  plugins: [
+    new webpack.optimize.CommonsChunkPlugin(/* chunkName= */'vendor', /* filename= */'vendor.js'),
+    new webpack.ProvidePlugin({
+      $: "jquery",
+      jQuery: "jquery"
+    }),
+    new webpack.DefinePlugin({ 'process.env.NODE_ENV': '"development"' }),
+    new extractTextPlugin('main.min.css')
+  ],
+  output: {
+    path: path.resolve('./client/dist/'),
+    filename: '[name].min.js'
+  },
+  module: {
+    preLoaders: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        loader: 'eslint-loader'
+      }
+    ],
+    loaders: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        loader: 'babel-loader',
+        query: {
+          presets: 'es2015'
+        }
+      },
+      {
+        test: /\.scss$/,
+        exclude: /node_modules/,
+        loader: extractTextPlugin.extract('style-loader','css-loader!autoprefixer-loader!sass-loader')
+      },
+      {
+        test: /\.css$/,
+        loader: extractTextPlugin.extract('style-loader', 'css-loader')
+      },
+      {
+        test: /\.(png|jpg)$/,
+        node_modules: /node_modules/,
+        loader: 'url-loader?limit=1000'
+      },
+      {test: /\.woff(2)?(\?v=\d+\.\d+\.\d+)?$/, loader: "url?limit=10000&mimetype=application/font-woff" },
+      {test: /\.ttf(\?v=\d+\.\d+\.\d+)?$/, loader: "url?limit=10000&mimetype=application/octet-stream" },
+      {test: /\.eot(\?v=\d+\.\d+\.\d+)?$/, loader: "file" },
+      {test: /\.svg(\?v=\d+\.\d+\.\d+)?$/, loader: "url?limit=10000&mimetype=image/svg+xml" }
+    ]
+  },
+  stats: {
+    colors: true
+  },
+  resolve: {
+    extensions: ['', '.js', '.es6']
+  }
+};
