2a34bce209dcc9dcbd19fc86ef47bfc12f86f44e
luciamv <calliope@Calliopes-iMac.local>
2016-01-28 12:44:02 -0600

    Delete road icons on map style

--- client/scripts/map_utils.js
+++ client/scripts/map_utils.js
@@ -97,6 +97,14 @@ var MapUtils = {
         "visibility": "on"
       }]
     }, {
+      "featureType": "road",
+      "elementType": "labels.icon",
+      "stylers": [
+          {
+              "visibility": "off"
+          }
+      ]
+    },{
       "featureType": "transit",
       "elementType": "all",
       "stylers": [{
